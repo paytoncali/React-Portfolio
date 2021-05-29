b1e85f88331e6e1381e981e480e816fcdaa45eed
Payton Whinnery <paytoncaliw@gmail.com>
2021-05-29 15:26:56 -0500

    changed max width of pictures to trouble shot pictures issue

--- src/components/Image/ProjectImg.jsx
+++ src/components/Image/ProjectImg.jsx
@@ -13,7 +13,7 @@ const ProjectImg = ({ filename, alt }) => (
               relativePath
               name
               childImageSharp {
-                fluid(maxWidth: 800) {
+                fluid(maxWidth: 700) {
                   ...GatsbyImageSharpFluid
                 }
               }
