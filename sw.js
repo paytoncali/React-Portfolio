2f3706b847bc105e3a3afa11ad67e29595f76b67
Payton Whinnery <paytoncaliw@gmail.com>
2022-06-15 14:24:01 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e9b3935d4a77935ef689.js"
+    "url": "webpack-runtime-db6f4ef56ab858aab678.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7cd7ce6760ec7b9eabf0a889d0022c18"
+    "revision": "80c4ad5357ed2a8769599da69e9e4fc6"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "8628e26b238e3f347de8f04f92f71952"
+    "revision": "8ca205d0472d7783027513c5235f4014"
   },
   {
     "url": "polyfill-57fa76c8ad53bd44c5a1.js"
